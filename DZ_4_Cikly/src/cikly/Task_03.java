1594bd4cb183c1437ec7effcf46033a141cfdf6d
Алексей Казаков <spovsem@gmail.com>
2022-06-30 14:10:51 +0700

    делаю Task3-4.1

--- DZ_4_Cikly/src/cikly/Task_03.java
+++ DZ_4_Cikly/src/cikly/Task_03.java
@@ -10,36 +10,49 @@ public class Task_03 {
 //		Задание 3
 //		Показать на экране все числа Армстронга в диапазоне от
 //		10 до 1 000 000.
-
-	    System.out.println("Введите положительное число: ");
-	    Scanner in = new Scanner(System.in);
-	    if(in.hasNextInt()) {
-	    	int number = in.nextInt();	    	
-	    	int numberLength = 0;
-	    	long temp = 1;
-	    	while (temp <= number) {
-	    	    numberLength++;
-	    	    temp = (temp << 3) + (temp << 1); // temp *= 10;
-	    	}
-	    	System.out.println(numberLength);
-	    	 
-	    	for (int i = 1; i <= numberLength; i++ ) {
-	    		int num1 = n1 % 10;
-	   		 int n = n1 / 10;
-	   		int num2 = n % 10;
-	   		n = n /10;
-	    		
-	    	int num1 = number / 100;
-	    	int num2 = num1 % 10;
-	    	int num3 = num1 /10;
-	    	System.out.printf("num1 - %d, num2 - %d, num3 = %d", num1, num2, num3);	    		
-	    	}
+    	
+	    int number = 1_000;
+	    for (int i = 150; i <= number; i++) {
+		    int temp = i;
+		    int y = 0;
+		    while (temp != 0) {
+		    	temp /= 10;
+		    	++y;
+		    }
+		   // System.out.println("число из " + y + "символов");
+		    
+		    int sumPower = 0;
+		    temp = i;
+		    while (temp != 0) {
+		    	int num = temp % 10;
+		    	if (num>0) {
+			    	for ( i = 1; i<=y; i++) {
+			    		num = num * num;
+			    	}
+		    		
+		    	}
+			    	else{
+			    		break;
+			    	}
+		    }
+		    if (sumPower == i) {
+		    	System.out.println("i = " + i);		    	
+		    }
+		    
+		    		
+	    }
+		System.out.println("Конец программы!");
+//	    for (int i = 1; i <= numberLength; i++ ) {
+////	    		int num1 = n1 % 10;
+////	   		 int n = n1 / 10;
+////	   		int num2 = n % 10;
+////	   		n = n /10;
+//	    		
+//	    	int num1 = number / 100;
+//	    	int num2 = num1 % 10;
+//	    	int num3 = num1 /10;
+//	    //	System.out.printf("num1 - %d, num2 - %d, num3 = %d", num1, num2, num3);	    		
+//	    }
 	    	
-	    }
-	    else {
-	    	System.out.println("Это не число!");
-	    }
-	    
-	}
-
+	} 
 }
--- DZ_4_Cikly/src/cikly/Task_04.java
+++ DZ_4_Cikly/src/cikly/Task_04.java
@@ -1,10 +1,16 @@
 package cikly;
 
+import java.util.Scanner;
+
 public class Task_04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	    System.out.println("Введите положительное число: ");
+	    Scanner in = new Scanner(System.in);
+	    if(in.hasNextInt()) {
+	    	int number = in.nextInt();	
 	}
 
 }
