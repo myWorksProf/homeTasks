c6cfae2d98e7224ed28de496666ebb0c90bb3828
Алексей Казаков <spovsem@gmail.com>
2022-07-03 22:36:56 +0700

    выполнил Task03

--- DZ_4_Cikly/src/cikly/Task_03.java
+++ DZ_4_Cikly/src/cikly/Task_03.java
@@ -11,48 +11,31 @@ public class Task_03 {
 //		Показать на экране все числа Армстронга в диапазоне от
 //		10 до 1 000 000.
     	
-	    int number = 1_000;
-	    for (int i = 150; i <= number; i++) {
-		    int temp = i;
+	    int number = 1_000_000;
+	    int i = 1;
+	    for (int currentNum  = 10; currentNum <= number; currentNum++) {
+		    int temp = currentNum;
 		    int y = 0;
 		    while (temp != 0) {
 		    	temp /= 10;
 		    	++y;
 		    }
-		   // System.out.println("число из " + y + "символов");
-		    
 		    int sumPower = 0;
-		    temp = i;
+		    temp = currentNum;
 		    while (temp != 0) {
+		    	int sumNum = 1; ;
 		    	int num = temp % 10;
-		    	if (num>0) {
-			    	for ( i = 1; i<=y; i++) {
-			    		num = num * num;
-			    	}
-		    		
-		    	}
-			    	else{
-			    		break;
-			    	}
+			    for (int j = y; j>0; j--) {
+			    	sumNum = sumNum * num;
+			    }
+			    sumPower += sumNum;
+		   		temp /= 10;
 		    }
-		    if (sumPower == i) {
-		    	System.out.println("i = " + i);		    	
-		    }
-		    
-		    		
+		    if (sumPower == currentNum) {
+		    	System.out.printf("%d = %d\n", i, sumPower);
+		    	i ++;
+		    }		    		    		
 	    }
 		System.out.println("Конец программы!");
-//	    for (int i = 1; i <= numberLength; i++ ) {
-////	    		int num1 = n1 % 10;
-////	   		 int n = n1 / 10;
-////	   		int num2 = n % 10;
-////	   		n = n /10;
-//	    		
-//	    	int num1 = number / 100;
-//	    	int num2 = num1 % 10;
-//	    	int num3 = num1 /10;
-//	    //	System.out.printf("num1 - %d, num2 - %d, num3 = %d", num1, num2, num3);	    		
-//	    }
-	    	
 	} 
 }
