886459e84b4a6e0ed6c1330c35112c1b6425bbdb
Алексей Казаков <spovsem@gmail.com>
2022-07-05 23:01:14 +0700

    выполнил Task-05

--- DZ_4_Cikly/src/cikly/Task_05.java
+++ DZ_4_Cikly/src/cikly/Task_05.java
@@ -1,10 +1,37 @@
 package cikly;
 
+import java.util.Scanner;
+
 public class Task_05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+//			Домашнее задание No 4.1
+//			Задание 5
+//  		является ли число, введенное с клавиатуры палиндромом (одинаково 
+//			читающееся в обоих направлениях). 
+		
+	    System.out.print("Введите положительное число: ");
+	    Scanner in = new Scanner(System.in);
+	    if(in.hasNextInt()) {
+	    	int inputNumber = in.nextInt();
+	    	in.close();
+	        int lastDigit,sum=0;
+	        int enumerator = inputNumber;
+	        
+	        while (enumerator > 0) {
+	        	lastDigit = enumerator % 10;
+	        	sum = sum * 10 +lastDigit;
+	        	enumerator = enumerator / 10;
+	        }
+	        
+	        if(sum == inputNumber) {
+	        	System.out.println("YES");
+	        }
+	        else {
+	        	System.out.println("NO");
+	        }
+	    }
+	    System.out.println("Конец программы!");
 	}
-
 }
--- DZ_4_Cikly/src/cikly/Task_10.java
+++ DZ_4_Cikly/src/cikly/Task_10.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class Task_10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	    System.out.println("Введите положительное число: ");
-	    Scanner in = new Scanner(System.in);
-	    if(in.hasNextInt()) {
-	    	int number = in.nextInt();	
+//		Домашнее задание No 4.1
+//		Задание 10
 
-	}
+    System.out.println("Введите положительное число: ");
+    Scanner in = new Scanner(System.in);
+    if(in.hasNextInt()) {
+    	int number = in.nextInt();	
+    
+    }
+    System.out.println("Конец программы!");	}
 
 }
